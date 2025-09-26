0b02cb2a0239d0fce7015be69978073a66d0d0c3
Austin <ausbratton@gmail.com>
2025-09-26 07:30:06 -0500

    Add function that stores input in library

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    
+    this.title = title;
+    this.author = author;
+}
+
+function addBookToLibrary() {
+    const title = prompt("Enter book title: ");
+    const author = prompt("Enter book author: ");
+
+    const newBook = new Book(title, author);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary();
