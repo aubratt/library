0ad994bd08a3811b41d358ca3ea4780fc413ae17
Austin <ausbratton@gmail.com>
2025-09-26 10:53:30 -0500

    Update display after user input

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Book(title, author) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    
+
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
@@ -18,4 +18,24 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary();
+function displayLibrary(library) {
+    for (let i = 0; i < library.length; i++) {
+        const container = document.getElementById("container");
+
+        const title = document.createElement("p");
+        title.style.color = "#E1B111";
+        title.textContent = library[i].title;
+
+        const author = document.createElement("p");
+        author.style.color = "#E1B111";
+        author.textContent = library[i].author;
+
+        container.appendChild(title);
+        container.appendChild(author);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    addBookToLibrary();
+    displayLibrary(myLibrary);
+});
